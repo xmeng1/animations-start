b57bd89a59b88cc2c037273380cdbd341a831971
Xin Meng <x.meng@outlook.com>
2017-09-06 23:47:33 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,73 @@
+import { Component } from '@angular/core';
+import {animate, state, style, transition, trigger} from "@angular/animations";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  animations:[
+    trigger('divState',[
+      state('normal',style({
+        'background-color':'red',
+        transform: 'translateX(0)'
+      })),
+      state('highlighted',style({
+        'background-color': 'blue',
+        transform: 'translateX(100px)'
+      })),
+      transition('normal <=> highlighted', animate(300)),
+      //transition('highlighted => normal', animate(800))
+    ]),
+    trigger('wildState',[
+      state('normal',style({
+        'background-color':'red',
+        transform: 'translateX(0) scale(1)'
+      })),
+      state('highlighted',style({
+        'background-color': 'blue',
+        transform: 'translateX(100px) scale(1)'
+      })),
+      state('shrunken',style({
+        'background-color': 'green',
+        transform: 'translateX(0px) scale(0.5)'
+      })),
+      transition('normal <=> highlighted', animate(300)),
+      transition('highlighted => normal', animate(800)),
+      transition('shrunken <=> *', [
+        style({
+          'background-color': 'orange'
+        }),
+        animate(1000,style({
+          borderRadius: '50px'
+        })),
+        animate(2000)
+      ])
+    //
+    ])
+  ]
+})
+
+/*animate(500, style({
+        borderRadius: '50px'
+      }))*/
+export class AppComponent {
+  state = 'normal';
+  wildState = 'normal';
+	list = ['Milk', 'Sugar', 'Bread'];
+
+	onAdd(item) {
+		this.list.push(item);
+	}
+
+	onDelete(item) {
+		this.list.splice(this.list.indexOf(item), 1);
+	}
+
+  onAnimate() {
+    this.state == 'normal'? this.state = 'highlighted': this.state= 'normal';
+    this.wildState == 'normal'? this.wildState = 'highlighted': this.wildState= 'normal';
+  }
+
+  onShrink() {
+    this.wildState = 'shrunken'
+  }
+}
